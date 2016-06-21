9aa5ae61bf960a77f857c53027b66e38978af35e
S. M. Abrar Jahin <abrarjahin@live.com>
2016-06-22 04:15:47 +0600

    Event Handeling Added

--- js/script.js
+++ js/script.js
@@ -14,6 +14,25 @@ $(function()
 		{
 			draggable: true
 		},
-	});
+	})
+		.bind("geocode:result", function(event, result)
+		{
+			console.log('Success');
+			console.log(result.formatted_address);
+			console.log( result.geometry.location.lat() );
+			console.log( result.geometry.location.lng() );
+			console.log(result);
+		})
+		.bind("geocode:error", function(event, status)
+		{
+			console.log('Error');
+			console.log(status);
+		})
+		.bind("geocode:multiple", function(event, results)
+		{
+			console.log('Multiple');
+			console.log(results.length);
+			console.log(results);
+		});
 	//var product_location_map = $("#find_product_location").geocomplete("map");	//Get the map for any more operation
 });
